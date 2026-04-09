30c61edc99fbc7b84b6875367226a06ce8644ef8
Jcmedinah <jean.medina0399@gmail.com>
2026-04-08 21:36:58 -0500

    Feature: se realiza el iniciador del pipeline

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+
+def main():
+    df_ventas = pd.read_csv("data/raw/ventas.csv")
+    df_productos = pd.read_csv("data/raw/ventas.csv")
+
+if __name__ == "__main__":
+    main()
