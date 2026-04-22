a97c20be703bfbcf1509e6c4ac1f7b4bef211a92
Jcmedinah <jean.medina0399@gmail.com>
2026-04-21 22:09:52 -0500

    Fix: Se ajusta el pipeline para analisis aleja

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ from modules.data_process import cargar_datos, limpiar_nulos, estandarizar_texto
 
 
 def main():
-    df_prestamos = cargar_datos("data/raw/articles.csv")
-    df_articulos = cargar_datos("data/raw/loans.csv")
+    df_prestamos = cargar_datos("data/raw/loans.csv")
+    df_articulos = cargar_datos("data/raw/articles.csv")
     df_prestamos = limpiar_nulos(df_prestamos)
     df_articulos = limpiar_nulos(df_articulos)
     
-    # df_prestamos = estandarizar_texto(df_prestamos, "status")
-    # df_articulos = estandarizar_texto(df_articulos, "name")
+    df_prestamos = estandarizar_texto(df_prestamos, "status")
+    df_articulos = estandarizar_texto(df_articulos, "name")
     
     df_merge = pd.merge(df_prestamos, df_articulos, on="item_id")
-    print(df_merge)
-    print("📊 Productos más frecuentes:")
-    print(df_articulos["name"].value_counts())
 
-    print("\n📊 Total de ventas por producto:")
-    ventas_por_producto = df_merge.groupby("nombre_producto")["cantidad"].sum()
-    print(ventas_por_producto)
-
-    print("\n📍 Ventas en Medellín:")
-    df_medellin = df_merge[df_merge["ciudad_cliente"] == "medellin"]
-    print(df_medellin)
 
     print("Productos más frecuentes:")
     print(df_articulos["name"].value_counts())
+
+    print("Total de prestamos por producto:")
+    prestamos_por_producto = df_merge.groupby("name")["item_id"].count()
+    print(prestamos_por_producto)
+
+    print("Prestamos de Silla ergonomica:")
+    df_sillas = df_merge[df_merge["name"] == "silla ergonómica"]
+    print(df_sillas)
+
+
 if __name__ == "__main__":
     main()
